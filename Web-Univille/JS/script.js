05a598b9a396c1da5e405a148addfaf3db1fb756
Rafael Dos Santos Pereira <33553306+radsrei@users.noreply.github.com>
2022-11-29 20:12:34 -0300

    Add files via upload

---
+++ Web-Univille/JS/script.js
@@ -0,0 +1,19 @@
+const message = 'Hello world' // Try edit me
+// Log to console
+console.log(message)
+
+for (var a = 0; a< 10; a++){
+    console.log(message)
+}
+
+var array = [1,2,3,4]
+
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array.length)
+
+for (var a =0; a< array.length; a++){
+    console.log("a =", array[a]);
+}
