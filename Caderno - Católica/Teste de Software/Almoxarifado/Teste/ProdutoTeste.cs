5ee1ac47f850fc8b939ad2b850932aa2bf4aef24
Rafael Dos Santos Pereira <33553306+radsrei@users.noreply.github.com>
2023-08-23 21:23:46 -0300

    Add files via upload

---
+++ Caderno - Católica/Teste de Software/Almoxarifado/Teste/ProdutoTeste.cs
@@ -0,0 +1,21 @@
+namespace Teste
+{
+    public class ProdutoTeste
+    {
+        /*
+         Eu, como almoxarife, preciso cadastrar produtos para que sejam
+        consumidos pelos funcionários da empresa
+
+        Critérios de Aceite:
+        - Um novo produto deve ter obrigatoriamente um codigo único, nome, saldo 0, custo 0 e unidade de medida
+        - As unidades de medida são: Pc, kg, m, g, l, cx
+        - Um produto pode ter uma descrição
+        - Um produto pode ter um código de barras EAN
+        */
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
